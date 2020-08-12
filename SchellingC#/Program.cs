b56d816c0190207e4bf0f7315dc3a33d4162dfee
acastellanos95 <acastellanosaldama@gmail.com>
2020-08-12 13:05:26 -0500

    C# project initiated

---
+++ SchellingC#/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SchellingC_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
